9a92a502b2b7712b7b6d4136f4029ad1b52ac649
Łukasz Plewa <lukasz.plewa@intel.com>
2022-12-15 16:43:55 +0100

    enable projectile

--- init.el
+++ init.el
@@ -62,7 +62,10 @@
 
 
 (straight-use-package 'projectile)
-;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
 
 (straight-use-package 'undo-tree)
 (global-undo-tree-mode)
@@ -142,7 +145,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values
+   '((eval progn
+	   (require 'projectile)
+	   (setq projectile-project-compilation-dir ".")
+	   (puthash
+	    (projectile-project-root)
+	    "cmake -B build --fresh" projectile-configure-cmd-map)
+	   (puthash
+	    (projectile-project-root)
+	    "cmake --build build" projectile-compilation-cmd-map))
+     (eval progn
+	   (require 'projectile)
+	   (setq projectile-project-compilation-dir ".")
+	   (puthash
+	    (projectile-project-root)
+	    "cmake -B build" projectile-configure-cmd-map)
+	   (puthash
+	    (projectile-project-root)
+	    "cmake --build build" projectile-compilation-cmd-map)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
